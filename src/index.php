100e73d2f05e6135d0a7b3c5ded08849c5a2c408
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-10-02 03:58:03 +0700

    Doc: add comment

--- src/index.php
+++ src/index.php
@@ -9,6 +9,16 @@ $dbPassword = $_ENV['DB_PASSWORD'];
 // Establish the PostgreSQL database connection
 $conn = pg_connect("host=$dbHost port=$dbPort dbname=$dbName user=$dbUser password=$dbPassword");
 
+/*
+ * if you get error make sure you install pdo pdo_pgsql pgsql extension
+ * see it in this link https://onesoftwaretester.wordpress.com/2018/05/22/docker-a-dockerfile-to-fix-the-call-to-undefined-function-pg_connect-error-when-using-php-with-postgres/
+*/
+
+/*
+ * postgres image maybe doesn't affect, it is use for development purposes
+ * but you can use your local postgres and this will affect
+ */
+
 if (!$conn) {
     die("Connection failed: " . pg_last_error());
 }

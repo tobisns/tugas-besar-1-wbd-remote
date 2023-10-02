c1766825144ebca469e78e5fb553f076edc6eb76
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-10-02 11:38:22 +0700

    Feat: Create Interface, Config, and Database

---
+++ src/app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+// Database
+define('HOST', $_ENV['DB_HOST']);
+define('DBNAME', $_ENV['DB_NAME']);
+define('USER', $_ENV['DB_USER']);
+define('PASSWORD', $_ENV['DB_PASSWORD']);
+define('PORT', $_ENV['DB_PORT']);
---
+++ src/app/core/Database.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace core;
+
+class Database
+{
+    private $host = HOST;
+    private $db_name = DBNAME;
+    private $user = USER;
+    private $password = PASSWORD;
+    private $port = PORT;
+
+    private $db_connection;
+
+    public function __construct(){
+        $conn_str = "host=$this->host port=$this->port dbname=$this->db_name user=$this->user password=$this->password";
+        $this->db_connection = pg_connect($conn_str);
+        if (!$this->db_connection) {
+            die("Connection failed: " . pg_last_error());
+        }
+    }
+
+    public function query($query){
+        $result = pg_query($this->db_connection, $query);
+        return $result;
+    }
+
+}

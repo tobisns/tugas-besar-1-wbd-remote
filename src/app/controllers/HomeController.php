f14f490d3d3d28cd07fd3265450db1301dcc50e3
arleenchr <13521059@mahasiswa.itb.ac.id>
2023-10-04 13:56:37 +0700

    Fix: Home Controller

--- src/app/controllers/HomeController.php
+++ src/app/controllers/HomeController.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../core/Controller.php';
 require_once __DIR__ . '/../core/Database.php';
 class HomeController extends Controller implements ControllerInterface{
     public function index(){
-        $loginView = $this->view('home', 'HomeView');
-        $loginView->render();
+        $homeView = $this->view('home', 'HomeView');
+        $homeView->render();
     }
 }

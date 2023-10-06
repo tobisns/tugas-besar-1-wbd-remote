af0f61fa839f3a5dfa510c12a8fd478ff52142e5
tobias ns <13521090@std.stei.itb.ac.id>
2023-10-06 14:45:34 +0700

    Feat: working pagination buttons at admin

--- src/app/components/admin/AddAlbumPage.php
+++ src/app/components/admin/AddAlbumPage.php
@@ -48,48 +48,55 @@
                     <?php endwhile; ?>
                 </div>
                 <div class="pagination">
-                    <button class="prev-button">
-                        <svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M7.875 0.9375L0 7.57875L7.875 14.0625V8.8125H21V6.1875H7.875V0.9375Z" fill="#74B18F"/>
-                        </svg>
-                        prev
-                    </button>
-                <?php if((int) $this->data['current_page'] > 5) : ?>
-                    <button class=pagination-button>1</button>
-                    <button class=pagination-button>2</button>
-                    <button class=pagination-button>...</button>
-                    <?php if((int) $this->data['total_page'] > $this->data['current_page'] + 5) : ?>
-                        <button class=pagination-button><?php echo $this->data['current_page'] - 2; ?></button>
-                        <button class=pagination-button><?php echo $this->data['current_page'] - 1; ?></button>
+                    <?php if($this->data['current_page'] > 1 ) : ?>
+                        <button class="prev-button">
+                            <svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M7.875 0.9375L0 7.57875L7.875 14.0625V8.8125H21V6.1875H7.875V0.9375Z" fill="#74B18F"/>
+                            </svg>
+                            prev
+                        </button>
                     <?php endif; ?>
-                <?php else : ?>
-                    <?php for ($i = 1; $i < min(max($this->data['current_page'], 6), $this->data['total_page']); $i++) : ?>
-                        <button class=pagination-button><?php echo  $i;?></button>
-                    <?php endfor; ?>
-                <?php endif; ?>
-                <?php if((int) $this->data['total_page'] > $this->data['current_page'] + 5) : ?>
-                    <?php if((int) $this->data['current_page'] > 5) : ?>
-                        <button class=pagination-button><?php echo max($this->data['current_page'],5); ?></button>
+                    <?php if($this->data['total_page'] > 10) : ?>
+                        <?php if($this->data['current_page'] <= 5) : ?>
+                            <?php for ($i = 1; $i <= 5; $i++) : ?>
+                                <button class=pagination-button><?php echo  $i;?></button>
+                            <?php endfor; ?>
+                        <?php else : ?>
+                            <button class=pagination-button>1</button>
+                            <button class=pagination-button>2</button>
+                            <button class=pagination-button>...</button>
+                            <button class=pagination-button><?php echo  min($this->data['total_page'] - 4,$this->data['current_page'])-2;?></button>
+                            <button class=pagination-button><?php echo  min($this->data['total_page'] - 4,$this->data['current_page'])-1;?></button>
+                        <?php endif; ?>
+                        <?php if(!($this->data['current_page'] <= 5 || $this->data['current_page'] >= $this->data['total_page'] - 4)) : ?>
+                            <button class=pagination-button><?php echo  $this->data['current_page'];?></button>
+                        <?php endif; ?>
+                        <?php if($this->data['current_page'] >= $this->data['total_page'] - 4) : ?>
+                            <?php for ($i = min($this->data['total_page'] - 4,$this->data['current_page']); $i <= $this->data['total_page']; $i++) : ?>
+                                <button class=pagination-button><?php echo  $i;?></button>
+                            <?php endfor; ?>
+                        <?php else : ?>
+                                <button class=pagination-button><?php echo  max(5,$this->data['current_page']) + 1;?></button>
+                                <button class=pagination-button><?php echo  max(5, $this->data['current_page']) + 2;?></button>
+                                <button class=pagination-button>...</button>
+                                <button class=pagination-button><?php echo  $this->data['total_page']-1;?></button>
+                                <button class=pagination-button><?php echo  $this->data['total_page'];?></button>
+                        <?php endif; ?>
+                    <?php else : ?>
+                        <?php for ($i = 1; $i <= $this->data['total_page']; $i++) : ?>
+                            <button class=pagination-button><?php echo  $i;?></button>
+                        <?php endfor; ?>
                     <?php endif; ?>
-                    <button class=pagination-button><?php echo max($this->data['current_page'],5) + 1; ?></button>
-                    <button class=pagination-button><?php echo max($this->data['current_page'],5) + 2; ?></button>
-                    <button class=pagination-button>...</button>
-                    <button class=pagination-button><?php echo $this->data['total_page'] + -1; ?></button>
-                    <button class=pagination-button><?php echo $this->data['total_page']; ?></button>
-                <?php elseif($this->data['total_page'] > 10) : ?>
-                    <?php for ($i =  $this->data['total_page'] - 6; $i <= $this->data['total_page']; $i++) : ?>
-                        <button class=pagination-button><?php echo  $i;?></button>
-                    <?php endfor; ?>
-                <?php endif; ?>
-                    <button class="next-button">
-                        next
-                        <svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M13.125 0.9375L21 7.57875L13.125 14.0625V8.8125H0V6.1875H13.125V0.9375Z" fill="#74B18F"/>
-                        </svg>
-                    </button>
+                    <?php if($this->data['current_page'] < $this->data['total_page']) : ?>
+                        <button class="next-button">
+                            next
+                            <svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M13.125 0.9375L21 7.57875L13.125 14.0625V8.8125H0V6.1875H13.125V0.9375Z" fill="#74B18F"/>
+                            </svg>
+                        </button>
+                    <?php endif; ?>
                 </div>
             <?php endif; ?>
         </div>
     </body>
-    
 </add_album>
--- src/public/javascripts/admin/admin_wrapper.js
+++ src/public/javascripts/admin/admin_wrapper.js
@@ -1,11 +1,19 @@
 const dynamicContainer = document.querySelector("#dynamic-content-container");
 const albumsButton = document.querySelector("#albums-button");
 const songsButton = document.querySelector("#songs-button");
+var nextButton = document.querySelector(".next-button");
+var prevButton = document.querySelector(".prev-button");
+var paginationButtons = document.querySelectorAll(".pagination-button");
 var albums = document.querySelectorAll(".album");
 var albumContainer = document.querySelector(".album-container");
 var albumState = {};
 
+var totalPage = 0;
+
 reallign_album();
+reload_page_button();
+get_current_album_page();
+console.log(paginationButtons);
 
 albumsButton &&
 albumsButton.addEventListener('change', (e) => {
@@ -22,6 +30,49 @@ songsButton.addEventListener('change', (e) => {
     }  
 });
 
+const updateAlbumPage = (page) => {
+    console.log('fired');
+    const xhr = new XMLHttpRequest();
+    xhr.open(
+        "GET",
+        `/public/admin/get_sub_div/albums/${page}`
+    );
+
+    xhr.send();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (this.status === 201) {
+                dynamicContainer.innerHTML = this.responseText;
+                albums = document.querySelectorAll(".album");
+                albumContainer = document.querySelector(".album-container");
+                paginationButtons = document.querySelectorAll(".pagination-button");
+
+                nextButton = document.querySelector(".next-button");
+                prevButton = document.querySelector(".prev-button");
+                
+
+                albumState = {};
+
+                reallign_album();
+                reload_page_button();                                                                                              
+
+                //set new url
+                const currentURL = window.location.href;
+                const parts = currentURL.split('/');
+                let partIndex = parts.indexOf('admin');
+
+                var newURL;
+
+                const urlUntilAdmin = parts.slice(0, partIndex + 1).join('/');
+                newURL = urlUntilAdmin+`/albums/${page}`;
+                history.pushState(null, null, newURL);
+            } else {
+                alert("An error occured, please try again!");
+            }
+        }
+    };
+}
+
 const updateContainer = (content) => {
     if(content == 'albums') {
         const xhr = new XMLHttpRequest();
@@ -34,7 +85,6 @@ const updateContainer = (content) => {
         xhr.onreadystatechange = function () {
             if (xhr.readyState === XMLHttpRequest.DONE) {
                 if (this.status === 201) {
-                    console.log(this.responseText);
                     dynamicContainer.innerHTML = this.responseText;
                     albums = document.querySelectorAll(".album");
                     albumContainer = document.querySelector(".album-container");
@@ -89,6 +139,36 @@ const updateContainer = (content) => {
     }
 };
 
+function get_current_album_page(){
+    const currentURL = window.location.href;
+    const parts = currentURL.split('/');
+    if(currentPage = parseInt(parts[parts.indexOf('albums') + 1])){
+        console.log(currentPage);
+        return currentPage;
+    }
+    return 1;
+}
+
+function reload_page_button(){
+    paginationButtons &&
+    paginationButtons.forEach(pageButton => {
+        totalPage = parseInt(pageButton.innerHTML) > totalPage ? parseInt(pageButton.innerHTML) : totalPage;
+        pageButton.addEventListener('click', () => {
+            console.log('fired');
+            updateAlbumPage(parseInt(pageButton.innerHTML));
+        });
+    });
+
+    prevButton &&
+    prevButton.addEventListener('click', () => {
+        updateAlbumPage(get_current_album_page() - 1);
+    })
+
+    nextButton &&
+    nextButton.addEventListener('click', () => {
+        updateAlbumPage(get_current_album_page() + 1);
+    })
+}
 
 albums &&
 window.addEventListener('resize', async () => {
